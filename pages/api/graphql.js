5077b7f2763b12bec7aded7ac9782a3fbbda01e3
Raja Krishna <rajakrishna214@gmail.com>
2020-07-01 16:58:43 -0500

    api-routes

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+	// console.log("hello");
+	res.status(200).json({
+		name: "John Doe",
+	});
+};
