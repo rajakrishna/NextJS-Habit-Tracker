8522f7ab59fdcb057cd7d9009ddf63425289e8da
Raja Krishna <rajakrishna214@gmail.com>
2020-07-02 14:55:34 -0500

    added-mongoDB

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const { parsed: localEnv } = require("dotenv").config();
 const webpack = require("webpack");
 
+// Pushing the env variables to next js config
 module.exports = {
 	webpack(config) {
 		config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
