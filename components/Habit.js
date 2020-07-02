5df2f16a95c85ef665f12fb4bcb956e9aaded039
Raja Krishna <rajakrishna214@gmail.com>
2020-07-01 23:56:59 -0500

    basic-interface-with-Habits

---
+++ components/Habit.js
@@ -0,0 +1,18 @@
+import HabitButton from "./HabitButton";
+
+const Habit = () => {
+	return (
+		<article>
+			<h3>Habit Title</h3>
+			<div>
+				<HabitButton />
+				<HabitButton />
+				<HabitButton />
+				<HabitButton />
+				<HabitButton />
+			</div>
+		</article>
+	);
+};
+
+export default Habit;
--- components/nav.js
+++ components/nav.js
@@ -1,14 +1,6 @@
 import React from "react";
 import Link from "next/link";
 
-const links = [
-	{ href: "https://zeit.co/now", label: "ZEIT" },
-	{ href: "https://github.com/zeit/next.js", label: "GitHub" },
-].map((link) => {
-	link.key = `nav-link-${link.href}-${link.label}`;
-	return link;
-});
-
 const Nav = () => (
 	<nav>
 		<ul>
@@ -16,15 +8,12 @@ const Nav = () => (
 				<Link href="/">
 					<a>Home</a>
 				</Link>
+			</li>
+			<li>
 				<Link href="/about">
 					<a>About</a>
 				</Link>
 			</li>
-			{links.map(({ key, href, label }) => (
-				<li key={key}>
-					<a href={href}>{label}</a>
-				</li>
-			))}
 		</ul>
 
 		<style jsx>{`
